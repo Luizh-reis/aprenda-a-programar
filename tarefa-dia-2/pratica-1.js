c4e849d2260157dcbc369863ddee745247100723
Luizh-reis <luizhenriquexavierreis@gmail.com>
2022-08-24 17:56:16 -0300

    alterandoOGithub

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,3 +2,12 @@ let nome = "Luiz"
 let email = "luizhenriquexavierreis@gmail.com"
 let profissao = "professor"
 let viagemDosSonhos = "lasVegas"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(viagemDosSonhos)
+
+let mensagem = "Ola, <luiz>  ! Somos aqui da agencia de turismo Viagem Dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade e " + idade + ", voce e " + profissao + " e o seu email e " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! ;D"
+
+console.log (mensagem)
+
