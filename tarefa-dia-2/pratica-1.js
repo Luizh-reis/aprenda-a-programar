c228900ea5156c3ca05056c1b4054b36a2be5cdb
Luizh-reis <luizhenriquexavierreis@gmail.com>
2022-08-25 18:36:30 -0300

    alterandoOGithub

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,6 +9,6 @@ console.log(profissao)
 console.log(idade)
 console.log(viagemDosSonhos)
 
-let mensagem = "Ola, " + nome + "! Somos aqui da agencia de turismo Viagem Dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade e " + idade + ", voce e " + profissao + " e o seu email e " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! ;D"
+let mensagem = `Ola, ${nome} ! Somos aqui da agencia de turismo Viagem Dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para ${viagemDosSonhos}. Confirma pra gente alguns dados? A sua idade e ${idade}, voce e ${profissao} e o seu email e ${email} ? Ficamos no aguardo. Muito obrigado e boa viagem! ;D`
 
-console.log (mensagem)
+console.log(mensagem)
