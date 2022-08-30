28f4bb032eaa1189517bd2b2f29e938156726256
Luizh-reis <luizhenriquexavierreis@gmail.com>
2022-08-29 21:20:02 -0300

    alterandoOGithub

--- tarefa-dia-2/pratica-4.js
+++ tarefa-dia-2/pratica-4.js
@@ -2,21 +2,21 @@ let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
 let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
 let produtoC = {"nome": "sandalia", "valor": 120.00, "internacional": false};
 
-if (produtoA.internacional == true) {
+if (produtoA.internacional) {
     console.log(`${produtoA.nome}  ${produtoA.valor * 1.2}`)
 
 } else {
     console.log(`${produtoA.nome}  ${produtoA.valor * 1.12}`)
 }
     
-if (produtoB.internacional == true) {
+if (produtoB.internacional) {
     console.log(`${produtoB.nome} ${produtoB.valor * 1.2}`)
 
 } else {
     console.log(`${produtoB.nome} ${produtoB.valor * 1.12}`)
 }
 
-if (produtoC.internacional == true) {
+if (produtoC.internacional) {
     console.log(`${produtoC.nome}  ${produtoC.valor * 1.2}`)
    
 } else {
